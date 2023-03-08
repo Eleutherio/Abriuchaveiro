fba57470248b4207046bc9e5d10cfeca797ba282
guifer em notebook <guifer.3097@@gmail.com>
2023-03-08 15:19:43 -0300

    update

--- Js/validacao.js
+++ Js/validacao.js
@@ -1,45 +1,5 @@
-const form = document.getElementById('form')
 
-const nomeInput = document.getElementById('nome')
-const telefoneInput = document.getElementById('fone')
-const emailInput = document.getElementById('email')
-const mensagemInput = document.getElementById('msg')
-const campos = document.querySelectorAll('.required')
-const spans = document.getElementsByClassName('.span__required');
-
-
-
-console.log (form, nomeInput, emailInput, telefoneInput, mensagemInput);
-
-form.addEventListener ("submit", (event) => {
-   event.preventDefault();
-
-    if (emailInput.value === "" || validaEmail (emailInput.value) || validaTelefone (telefoneInput)) {
-        ("Por favor, ensira seus dados");
-        return;
-    } 
-
-    
-     form.submit();
-});
-
-function validaTelefone (tel) {
-    const telregex = new RegExp (
-        /^[0-9.-]$/ 
-    ); if (telregex.test(tel))
-    return true;
- else return false;
-}
-function validaEmail (email){
-    const emailRegex = new RegExp (
-        /^[a-zA-Z0-9._-]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/
-    ); if (emailRegex.test(email))
-            return true;
-        else {
-            return false;
-    }
-} 
-function requiredError(index){
-    campos[index].style.border = '2px solid red';
-    spans[index].style.display = block;
-}
+$('.nav__menu__itens a').mouse(function() {
+    $('.nav__menu__itens > a').removeClass('active');
+    $(this).addClass('active'); 
+}); 
